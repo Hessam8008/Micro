39059459036c5c3100d1b7ba13013aac76c0062b
Hessam <hessam8008@yahoo.com>
2023-11-25 11:05:57 +0330

    Update Product entity.

--- Services/Catalog/Catalog.Api/Entities/Product.cs
+++ Services/Catalog/Catalog.Api/Entities/Product.cs
@@ -7,13 +7,17 @@ public class Product
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public int Id { get; set; }
+    public string Id { get; set; }
 
     [BsonElement("Name")] public string? Name { get; set; }
 
-    public string? Catalog { get; set; }
+    public string? Category { get; set; }
 
     public string? Summary { get; set; }
 
     public string? Description { get; set; }
+
+    public string? ImageFile { get; set; }
+
+    public decimal Price { get; set; }
 }
