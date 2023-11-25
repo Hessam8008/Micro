9636a522d35ee9d22a9fc903b9e612d4ae6f18ff
Hessam <hessam8008@yahoo.com>
2023-11-25 10:57:39 +0330

    Implement ICatalogContext in CatalogContext.

---
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IConfiguration Configuration)
+    {
+        var connectionString = Configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+        var databaseName = Configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+        var collectionName = Configuration.GetValue<string>("DatabaseSettings:CollectionName");
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(databaseName);
+        Products = database.GetCollection<Product>(collectionName);
+    }
+
+    public IMongoCollection<Product> Products { get; }
+}
